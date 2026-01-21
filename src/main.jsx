a43a0b8241dcb5918ea7260c833c0c1bd6dd27a8
Oleh Salikovskyi <olehsalikovskyi@MacBook-Air-Oleh.local>
2026-01-22 00:54:25 +0100

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import "./styles.css";
 import App from './App.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode> 
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
